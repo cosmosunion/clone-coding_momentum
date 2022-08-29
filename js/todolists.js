fc43fb1713367480fc0083e7581e430e5f26dc71
cosmosunion <105220575+cosmosunion@users.noreply.github.com>
2022-08-29 15:31:52 +0900

    final

--- js/todolists.js
+++ js/todolists.js
@@ -2,20 +2,25 @@ const toDoForm = document.querySelector(".to-do-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoLists = document.querySelector("#lists");
 
-const toDoArray = [];
+const TODOS_KEY = "todos";
+let toDoArray = []; /*새로 고쳤을때 기존 데이터 로드하기 위해 let*/
 
 function saveToDo() {
-  localStorage.setItem("todos", JSON.stringify(toDoArray));
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDoArray));
 }
 
 function deleteToDo(click) {
-  click.target.parentElement.remove();
+  const li = click.target.parentElement;
+  li.remove();
+  toDoArray = toDoArray.filter((item) => item.id !== parseInt(li.id));
+  saveToDo();
 }
 
-function addToDo(newToDo) {
+function addToDo(newToDoObj) {
   const li = document.createElement("li");
+  li.id = newToDoObj.id;
   const span = document.createElement("span");
-  span.innerText = newToDo;
+  span.innerText = newToDoObj.text;
   li.appendChild(span);
   const btn = document.createElement("button");
   btn.innerText = "X";
@@ -28,9 +33,21 @@ function paintToDo(event) {
   event.preventDefault();
   const newToDo = toDoInput.value;
   toDoInput.value = "";
-  toDoArray.push(newToDo);
-  addToDo(newToDo);
+  const newToDoObj = {
+    id: Date.now(),
+    text: newToDo,
+  };
+  toDoArray.push(newToDoObj);
+  addToDo(newToDoObj);
   saveToDo();
 }
 
 toDoForm.addEventListener("submit", paintToDo);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDoArray = parsedToDos;
+  parsedToDos.forEach(addToDo);
+}
